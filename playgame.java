f84c5b3ff5c7754e21eaac4e98176da10af936d4
petrat1 <petra.tsakopoulos26@ycdsbk12.ca>
2025-06-12 03:08:08 -0400

    Extra requirement
    Added system.out.println extra requirement, and fixed minor errors in the code.

--- playgame.java
+++ playgame.java
@@ -1,115 +1,127 @@
-import arc.Console;
-import arc.TextInputFile;
+import arc.*;
 import java.awt.Color;
 import java.awt.Font;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.io.IOException;
 import java.util.Random;
 import java.util.ArrayList;
 
 public class playgame {
-    public Console con;
-    public Random rand = new Random();
+	public Console con;
+	public Random rand = new Random();
 
-    public playgame() {
-        con = new Console("Play Game", 800, 700); 
+	public playgame() {
+		con = new Console("Play Game", 800, 700);
+	}
+
+	public void start() {
+		setGraphics();
+
+		con.setDrawColor(Color.WHITE);
+		con.setDrawFont(new Font("Arial", Font.PLAIN, 20));
+		con.drawString("Enter your name:", 280, 300);
+		String strPlayerName = con.readLine();
+		System.out.println("Debug: Player name entered: " + strPlayerName);
+
+
+		TextInputFile fileQuizList = new TextInputFile("quizzes.txt");
+		String[] strQuizzes = new String[100];
+		int intQuizCount = 0;
+
+		while (fileQuizList.eof() != true) {
+			String strLine = fileQuizList.readLine();
+			if (strLine != null && !strLine.equals("")) {
+				strQuizzes[intQuizCount] = strLine.trim();
+				intQuizCount++;
+            }
+        }
+
+		setGraphics();
+		con.setDrawColor(Color.WHITE);
+		con.setDrawFont(new Font("Arial", Font.PLAIN, 20));
+		con.drawString("Available quizzes:", 280, 100);
+
+		for (int intI = 0; intI < intQuizCount; intI++) {
+			con.drawString((intI + 1) + ". " + strQuizzes[intI], 300, 140 + intI * 30);
+        }
+
+		con.drawString("Type the name of the quiz file you want to play (ex: countries.txt):", 150, 140 + intQuizCount * 30 + 20);
+		String strChosenQuiz = con.readLine().trim().toLowerCase();
+		System.out.println("Debug: Chosen quiz file: " + strChosenQuiz);
+
+		String[][] strQuiz = loadQuizFile(strChosenQuiz);
+		if (strQuiz == null) {
+			con.println("Error loading quiz file.");
+			return;
+        }
+
+		for (int intI = 0; intI < strQuiz.length; intI++) {
+			strQuiz[intI][6] = Integer.toString(rand.nextInt(100) + 1);
+        }
+
+		shuffleArray(strQuiz);
+		playQuiz(strPlayerName, strChosenQuiz, strQuiz);
     }
 
-    public void start() {
-        setGraphics();
+	public String[][] loadQuizFile(String strFileName) {
+		ArrayList<String[]> listLines = new ArrayList<>();
+		TextInputFile fileQuiz = new TextInputFile(strFileName);
 
-        con.println("Enter your name:");
-        String strPlayerName = con.readLine();
+		while (fileQuiz.eof() != true) {
+			String strQuestion = fileQuiz.readLine();
+			String strA = fileQuiz.readLine();
+            String strB = fileQuiz.readLine();
+            String strC = fileQuiz.readLine();
+            String strD = fileQuiz.readLine();
+            String strAnswer = fileQuiz.readLine();
 
-        String[] strQuizzes = {"countries", "famousmovies", "foodandcooking", "inventors", "space"};
-        con.println("\nAvailable quizzes:");
-        for (int intI = 0; intI < strQuizzes.length; intI++) {
-            con.println((intI + 1) + ". " + strQuizzes[intI]);
-        }
-
-        con.println("\nType the name of the quiz file you want to play (ex: countries):");
-        String strChosenQuiz = con.readLine().trim().toLowerCase();
-
-        String[][] strQuiz = loadQuizFile(strChosenQuiz);
-        if (strQuiz == null) {
-            con.println("Error loading quiz file! Returning to main menu...");
-            return;
-        }
-
-        for (int intI = 0; intI < strQuiz.length; intI++) {
-            strQuiz[intI][6] = Integer.toString(rand.nextInt(100) + 1);
-        }
-
-        shuffleArray(strQuiz);
-        playQuiz(strPlayerName, strChosenQuiz, strQuiz);
-    }
-
-    public String[][] loadQuizFile(String strFileName) {
-        java.util.List<String[]> listLines = new ArrayList<>();
-
-        try {
-            TextInputFile quizFile = new TextInputFile(strFileName + ".txt");
-
-            while (true) {
-                String strQuestion = quizFile.readLine();
-                String strA = quizFile.readLine();
-                String strB = quizFile.readLine();
-                String strC = quizFile.readLine();
-                String strD = quizFile.readLine();
-                String strAnswer = quizFile.readLine();
-
-                if (strQuestion == null || strA == null || strB == null || strC == null || strD == null || strAnswer == null) {
-                    break;
-                }
-
-                String[] strRow = new String[7];
-                strRow[0] = strQuestion;
-                strRow[1] = strA;
-                strRow[2] = strB;
-                strRow[3] = strC;
-                strRow[4] = strD;
-                strRow[5] = strAnswer;
-                strRow[6] = "";
-
-                listLines.add(strRow);
+if(strQuestion == null || strA == null || strB == null || strC == null || strD == null || strAnswer == null){
+                break;
             }
 
-        } catch (Exception e) {
-            return null;
+            String[] strRow = new String[7];
+            strRow[0] = strQuestion;
+            strRow[1] = strA;
+            strRow[2] = strB;
+            strRow[3] = strC;
+            strRow[4] = strD;
+            strRow[5] = strAnswer;
+            strRow[6] = "";
+
+            listLines.add(strRow);
         }
 
         return listLines.toArray(new String[0][0]);
     }
 
-    public void shuffleArray(String[][] arr) {
-        for (int intI = arr.length - 1; intI > 0; intI--) {
+    public void shuffleArray(String[][] strArr) {
+        for (int intI = strArr.length - 1; intI > 0; intI--) {
             int intJ = rand.nextInt(intI + 1);
-            String[] strTemp = arr[intI];
-            arr[intI] = arr[intJ];
-            arr[intJ] = strTemp;
+            String[] strTemp = strArr[intI];
+            strArr[intI] = strArr[intJ];
+            strArr[intJ] = strTemp;
         }
     }
 
     public void playQuiz(String strPlayerName, String strQuizName, String[][] strQuiz) {
         int intScore = 0;
+        boolean boolCheatActive = strPlayerName.equalsIgnoreCase("statitan");
         int intTotalQuestions = strQuiz.length;
 
         for (int intI = 0; intI < intTotalQuestions; intI++) {
             con.clear();
             setGraphics();
 
-            int intPercentSoFar = (intI == 0) ? 0 : (intScore * 100 / intI);
+            int intPercentSoFar = (intI == 0) ? (boolCheatActive ? 50 : 0) : (intScore * 100) / intI;
+
             con.setDrawColor(Color.WHITE);
             con.setDrawFont(new Font("Arial", Font.PLAIN, 20));
             con.drawString("Player: " + strPlayerName + "   Quiz: " + strQuizName + "   Score: " + intPercentSoFar + "%", 20, 40);
 
             con.setDrawFont(new Font("Arial", Font.PLAIN, 18));
-            con.drawString("Q" + (intI + 1) + ": " + strQuiz[intI][0], 20, 100);
-            con.drawString("A. " + strQuiz[intI][1], 20, 140);
-            con.drawString("B. " + strQuiz[intI][2], 20, 180);
-            con.drawString("C. " + strQuiz[intI][3], 20, 220);
-            con.drawString("D. " + strQuiz[intI][4], 20, 260);
+            con.drawString((intI + 1) + ": " + strQuiz[intI][0], 20, 100);
+            con.drawString(strQuiz[intI][1], 20, 140);
+            con.drawString(strQuiz[intI][2], 20, 180);
+            con.drawString(strQuiz[intI][3], 20, 220);
+            con.drawString(strQuiz[intI][4], 20, 260);
 
             con.setDrawFont(new Font("Arial", Font.ITALIC, 18));
             con.drawString("Enter your answer (A, B, C, D):", 20, 310);
@@ -144,30 +156,20 @@ public class playgame {
         con.drawString("Quiz: " + strQuizName, 20, 120);
         con.drawString("Score: " + intPercent + "%", 20, 160);
 
-        saveResultToLeaderboard(strPlayerName, strQuizName, intPercent);
+        TextOutputFile fileLeaderboard = new TextOutputFile("leaderboard.txt", true);
+        fileLeaderboard.println(strPlayerName);
+        fileLeaderboard.println(strQuizName);
+        fileLeaderboard.println(Integer.toString(intPercent));
 
         con.setDrawFont(new Font("Arial", Font.ITALIC, 18));
-        con.drawString("Press Enter to return to main menu...", 20, 220);
         con.readLine();
     }
 
-    public void saveResultToLeaderboard(String strPlayerName, String strQuizName, int intPercent) {
-        try (PrintWriter out = new PrintWriter(new FileWriter("leaderboard.txt", true))) {
-            out.println(strPlayerName + "," + strQuizName + "," + intPercent + "%");
-        } catch (IOException e) {
-            con.println("Error saving leaderboard data.");
-        }
-    }
-
     public void setGraphics() {
-        con.setDrawColor(new Color(30, 30, 60)); 
+        con.setDrawColor(new Color(30, 30, 60));
         con.fillRect(0, 0, 800, 700);
-        con.setDrawColor(Color.WHITE);
-        con.setDrawFont(new Font("Arial", Font.PLAIN, 18));
     }
 }
-
-
 
 
 
